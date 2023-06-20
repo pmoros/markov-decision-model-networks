5d0c7a60bfb023c9499cedce1f6d9c357f849c13
Paul Moros Anacona <pmoros@outlook.com>
2023-06-19 17:17:12 -0700

    refactor: launch runs in parallel

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sync"
 
 	"github.com/pmoros/markov-decision-model-networks/pkg/model"
 	"github.com/pmoros/markov-decision-model-networks/pkg/policy"
@@ -13,6 +14,7 @@ func main() {
 	// Define a flag for the policyType
 	policyTypeFlag := flag.Int("policyType", 0, "policy type (0-Default, 1-TypeA, 2-TypeB, 3-TypeC)")
 	timesToRunFlag := flag.Int("timesToRun", 1, "times to run the simulation")
+	showPolicyFlag := flag.Bool("showPolicy", false, "show the policy")
 
 	// Parse the command-line flags
 	flag.Parse()
@@ -21,56 +23,68 @@ func main() {
 	policyType := model.PolicyType(*policyTypeFlag)
 
 	// Print whether the policy is fixed, randomized or iterated
-	switch policyType {
-	case model.Fixed:
-		fmt.Println("Fixed policy")
-	case model.Randomized:
-		fmt.Println("Randomized policy")
-	case model.Iterated:
-		fmt.Println("Iterated policy")
-	default:
-		fmt.Println("Unknown policy")
+	if *showPolicyFlag {
+		switch policyType {
+		case model.Fixed:
+			fmt.Println("Fixed policy")
+		case model.Randomized:
+			fmt.Println("Randomized policy")
+		case model.Iterated:
+			fmt.Println("Iterated policy")
+		default:
+			fmt.Println("Unknown policy")
+		}
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(*timesToRunFlag)
 
 	for i := 0; i < *timesToRunFlag; i++ {
+		go func() {
+			// policyType := model.Fixed // model.Fixed, model.Randomized or model.Iterated
+			policy := policy.GeneratePolicy(policyType)
+			cells := [][]model.Cell{
+				{
+					model.NewCell(0, model.Clear, -0.04),
+					model.NewCell(1, model.Clear, -0.04),
+					model.NewCell(2, model.Clear, -0.04),
+					model.NewCell(3, model.Goal, 1.0),
+				},
+				{
+					model.NewCell(4, model.Clear, -0.04),
+					model.NewCell(5, model.Blocked, 0),
+					model.NewCell(6, model.Clear, -0.04),
+					model.NewCell(7, model.Goal, -1.0),
+				},
+				{
+					model.NewCell(8, model.Clear, -0.04),
+					model.NewCell(9, model.Clear, -0.04),
+					model.NewCell(10, model.Clear, -0.04),
+					model.NewCell(11, model.Clear, -0.04),
+				},
+			}
+			agent := model.Agent{
+				TransitionModel: model.Transition{
+					model.Forward:     0.8,
+					model.RotateLeft:  0.1,
+					model.RotateRight: 0.1,
+					model.Back:        0.0, // no back movements on this example
+				},
+				InitialCell: model.Coords{0, 2},
+				Energy:      0,
+			}
 
-		// policyType := model.Fixed // model.Fixed, model.Randomized or model.Iterated
-		policy := policy.GeneratePolicy(policyType)
-		cells := [][]model.Cell{
-			{
-				model.NewCell(0, model.Clear, -0.04),
-				model.NewCell(1, model.Clear, -0.04),
-				model.NewCell(2, model.Clear, -0.04),
-				model.NewCell(3, model.Goal, 1.0),
-			},
-			{
-				model.NewCell(4, model.Clear, -0.04),
-				model.NewCell(5, model.Blocked, 0),
-				model.NewCell(6, model.Clear, -0.04),
-				model.NewCell(7, model.Goal, -1.0),
-			},
-			{
-				model.NewCell(8, model.Clear, -0.04),
-				model.NewCell(9, model.Clear, -0.04),
-				model.NewCell(10, model.Clear, -0.04),
-				model.NewCell(11, model.Clear, -0.04),
-			},
-		}
-		agent := model.Agent{
-			TransitionModel: model.Transition{
-				model.Forward:     0.8,
-				model.RotateLeft:  0.1,
-				model.RotateRight: 0.1,
-				model.Back:        0.0, // no back movements on this example
-			},
-			InitialCell: model.Coords{0, 2},
-			Energy:      0,
-		}
+			scenarioGrid := util.CreateScenario(cells, policy, agent)
+			energyLevel := util.RunScenario(scenarioGrid)
 
-		scenarioGrid := util.CreateScenario(cells, policy, agent)
-		energyLevel := util.RunScenario(scenarioGrid)
+			if *showPolicyFlag {
+				fmt.Println(policy)
+			}
+			fmt.Println(energyLevel)
 
-		fmt.Println(policy)
-		fmt.Println(energyLevel)
+			wg.Done()
+		}()
 	}
+
+	wg.Wait()
 }
